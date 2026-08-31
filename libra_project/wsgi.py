006f3520f7d226616acd29c6baaa3a6b6df43d89
Om Yadav <omryadav9@gmail.com>
2026-09-01 03:23:08 +0530

    Add Vercel configuration and app entrypoint

--- libra_project/wsgi.py
+++ libra_project/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'libra_project.settings')
 
 application = get_wsgi_application()
+app = application
